79f4dbaf211b2512e46f73a45f062351309e9c6a
kushagra kataria <kushagrakataria19@gmail.com>
2025-01-11 15:05:22 +0530

    edited environment variables

--- src/connecting_db.js
+++ src/connecting_db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose"); 
 
-mongoose.connect("mongodb+srv://kushagra_kataria:hello1234@cluster0.avfati0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() =>{
+mongoose.connect(process.env.MONGODB_URL).then(() =>{
     console.log(`mongo db is connected successfully`); 
 }).catch((err) =>{
     console.log(err); 
